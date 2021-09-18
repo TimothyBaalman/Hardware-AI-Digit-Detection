22d9ccf123e5cc3df5ebea74a37493835384895b
TimothyBaalman <53982931+TimothyBaalman@users.noreply.github.com>
2021-09-18 13:00:41 -0500

    created a json writer.

---
+++ PyTorch-Training/json_writer.py
@@ -0,0 +1,42 @@
+import json
+import os
+import numpy as np
+
+path_to_dir = os.getcwd() + "/PyTorch-Training"
+
+class NumpyEncoder(json.JSONEncoder):
+	""" Special json encoder for numpy types """
+	def default(self, obj):
+		if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
+			np.int16, np.int32, np.int64, np.uint8,
+			np.uint16, np.uint32, np.uint64)):
+			return int(obj)
+		
+		elif isinstance(obj, (np.float_, np.float16, np.float32,np.float64)):
+			return float(obj)
+
+		elif isinstance(obj, (np.ndarray,)):
+			return obj.tolist()
+
+		return json.JSONEncoder.default(self, obj)
+
+def write_best_results(json_data):
+	try:
+		with open(f"{path_to_dir}/best_results.json", "r+") as file:
+			file.seek(0)
+			json.dump(json_data, file, indent=3, cls=NumpyEncoder)
+	except:
+		print("Couldn't Write to file")
+
+def add_to_history_best(json_data):
+	try:
+		with open(f"{path_to_dir}/history_best_results.json", "r+") as file:
+			file_data = json.load(file)
+			try:
+				file_data["results"].append(json_data)
+			except:
+				file_data["results"] = [json_data]
+			file.seek(0)
+			json.dump(file_data, file, indent=3, cls=NumpyEncoder)
+	except:
+		print("couldn't write to file")
