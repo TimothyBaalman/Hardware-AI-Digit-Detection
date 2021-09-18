e2a9958eb714459b9e203fb386214c46e33dba9b
TimothyBaalman <53982931+TimothyBaalman@users.noreply.github.com>
2021-09-18 01:02:57 -0500

    Create test.py

---
+++ PyTorch-Training/test.py
@@ -0,0 +1,19 @@
+best_results = {
+	"learning_rate": 0,
+	"batch_size": 0, 
+	"to_shuffle": False,
+	"loss": 100,
+	"accuracy": 0,
+	"fc_weights":[23,23],
+	"fc_bias":[],
+	"out_weights":[],
+	"out_bias":[]
+}
+
+print(best_results)
+
+best_results["accuracy"] = 50
+weights = [[50, 978, 2323, 23545], [32,32, 3563, 6734]]
+best_results["fc_weights"] = weights
+
+print(best_results)
