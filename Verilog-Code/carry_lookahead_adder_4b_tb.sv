56a42ae98db2be433e91e0523dfc1a31bed050cf
TimothyBaalman <53982931+TimothyBaalman@users.noreply.github.com>
2021-10-31 00:26:57 -0500

    Added testbench for cla

---
+++ Verilog-Code/carry_lookahead_adder_4b_tb.sv
@@ -0,0 +1,58 @@
+module cla_4b_tb;
+	reg [3:0] a_in, b_in;
+	reg carry_in;
+	wire [3:0] sum;
+	wire carry_out;
+
+	cla_4b cla0(
+		.a(a_in), .b(b_in), .c_in(carry_in),
+		.s(sum), .c_out(carry_out)
+	);
+
+	initial begin
+		$dumpfile("dump.vcd"); $dumpvars;
+
+		#1;
+		a_in = 4'b1111;
+		b_in = 4'b0000;
+		carry_in = 0;
+
+		#1;
+		a_in = 4'b0000;
+		b_in = 4'b1111;
+		carry_in = 0;
+
+		#1;
+		a_in = 4'b1010;
+		b_in = 4'b0101;
+		carry_in = 0;
+
+		#1;
+		a_in = 4'b1111;
+		b_in = 4'b1111;
+		carry_in = 0;
+
+		#1;
+		a_in = 4'b1111;
+		b_in = 4'b0000;
+		carry_in = 1;
+
+		#1;
+		a_in = 4'b0000;
+		b_in = 4'b1111;
+		carry_in = 1;
+
+		#1;
+		a_in = 4'b0101;
+		b_in = 4'b1010;
+		carry_in = 1;
+
+		#1;
+		a_in = 4'b1111;
+		b_in = 4'b1111;
+		carry_in = 1;
+
+		#1 $finish;
+	end
+
+endmodule
