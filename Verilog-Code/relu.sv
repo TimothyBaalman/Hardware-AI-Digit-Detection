e1c3ae67a575414b9d66cd0bf669f105c30b7500
TimothyBaalman <53982931+TimothyBaalman@users.noreply.github.com>
2021-11-01 00:01:13 -0500

    Fixed for 32 bit.

--- Verilog-Code/relu.sv
+++ Verilog-Code/relu.sv
@@ -1,9 +1,9 @@
 module relu(
-	input [63:0] r_in,
-	output [63:0] r_out
+	input [32:0] r_in,
+	output [32:0] r_out
 );
 	//If the signed bit is 0 we have a positive number so output it. 
 	// if not output 0. 
-	assign r_out = (r_in[63] == 0) ? r_in : 64{1b'0};
+	assign r_out = (r_in[32] == 0) ? r_in : 32{1b'0};
 	
 endmodule
