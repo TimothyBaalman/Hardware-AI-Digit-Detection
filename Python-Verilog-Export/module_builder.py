28454f1b212d4d4a138c2dfa9ef6c886e8fc5748
TimothyBaalman <53982931+TimothyBaalman@users.noreply.github.com>
2021-11-15 11:19:42 -0600

    Needs testing but finished?

--- Python-Verilog-Export/module_builder.py
+++ Python-Verilog-Export/module_builder.py
@@ -1,6 +1,8 @@
+from _typeshed import Self
 from full_adder_n_bit import fa_operations
 class Module_Builder():
-   def __init__(self, module_type, n_bits = 1, input_vals = [], output_vals = []):
+   def __init__(self, output_file, module_type, n_bits = 1, input_vals = [], output_vals = []):
+      self.file = output_file
       self.module_name = module_type + f"_{n_bits}b"
       self.n = n_bits
       self.input_vals = input_vals
@@ -17,6 +19,8 @@ class Module_Builder():
          '''self.build_2c_multiplier_nb()'''
       else:
          print(f"No module creation implemented for {module_type}")
+      
+      self.output_base()
   
    def build_base(self):
       self.base = ""
@@ -58,17 +62,40 @@ class Module_Builder():
          self.base += operation
       self.base += f"endmodule // for module {self.module_name}"
       
+   def output_base(self):
+      for data in self.base:
+         self.file.write(data)
+      
+      # Free memory
+      del(self.base)
 
+   # Passed in values are a tuple array where the first index is the matching name from base and the second is the bit name to link to the base
    def use_module(self, passed_in_inputs = [], pass_in_outputs = []):
       output = f"{self.module_name} {self.module_name}{self.increment_used_name}(\n"
+      in_size = len(self.input_vals)
+      out_size = len(self.output_vals)
+      # Possibly change to assume the indexes should be matching instead of matching names and just past in the name of 
+      for in_name, _ in self.input_vals:
+         for name, passed_in_name in passed_in_name:
+            if(name == in_name):
+               if(in_size == 1):
+                  output += f".{name}({passed_in_name})\n"
+               else:
+                  output += f".{name}({passed_in_name}) "
+               in_size -= 1
+               break
+
+      for out_name, _ in self.output_vals:
+         for name, passed_in_name in passed_in_name:
+            if(name == in_name):
+               if(out_size == 1):
+                  output += f".{name}({passed_in_name})\n"
+               else:
+                  output += f".{name}({passed_in_name}) "
+               out_size -= 1
+               break
+      self.file.write(output)
       self.increment_used_name += 1
 
-      #assuming indexes match up 
-      for index, inputs in enumerate(self.input_vals):
-         name = inputs[0]
-         # output last ref doesn't need a comma
-         output += f".{name}({passed_in_inputs[index]})"
-         '''Will be used to create names for this module'''
-
 fa_1b = Module_Builder("fa", 1, [("a", 1), ("b", 1), ("c_in", 1)], [("s", 1), ("c_out", 1)])
-print(fa_1b.use_module())
+print(fa_1b.use_module([("a","bit0"), ("b", "bit1"), ("c_in", "bit2")], [("s", "bit_O_0"), ("c_out", "bit_O_1")]))
