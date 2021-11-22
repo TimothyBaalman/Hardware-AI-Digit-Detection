82c6ac0bb7e262d80d6797c43a226c51bc8031e8
TimothyBaalman <53982931+TimothyBaalman@users.noreply.github.com>
2021-11-22 13:16:10 -0600

    Created functions for returning a string for the use case of a fa and ha if -1 it assumes there it's not an array value.

--- Python-Verilog-Export/full_adder_n_bit.py
+++ Python-Verilog-Export/full_adder_n_bit.py
@@ -1,18 +1,34 @@
-def fa_operations(n, inputs, outputs):
+from functions import use_fa
+
+def fa_operations(n, inputs, outputs, submodule):
 	output = []
-	#TODO pass in the sub module to get it's input
+	
 	a, b, c_in = inputs
 	s, c_out = outputs
-
+	ci_out = "ci_out"
 	if(n > 1):
-		output.append(f"	wire [{n-2}:0] ci_out;\n\n")
+		in_a, in_b, in_c_in = submodule[0].input_names
+		out_s, out_c_out = submodule[0].output_names
+		output.append(f"	wire [{n-2}:0] {ci_out};\n\n")
 		for i in range(n):
 			if(i == 0):
-				output.append(f"	fa_1b fa{i}(.a({a}[{i}]), .b({b}[{i}]), .c_in({c_in}), .c_out(ci_out[{i}]), .s({s}[{i}]));\n")
+				output.append(use_fa(i , (in_a, a, i), (in_b, b, i), (in_c_in, c_in, -1), (out_s, s, i), (out_c_out, ci_out, i)))
+				# output.append(f"	fa_1b fa{i}(\n")
+				# output.append(f"		.{in_a}({a}[{i}]), .{in_b}({b}[{i}]), .{in_c_in}({c_in}), //Inputs\n")
+				# output.append(f"		.{out_s}({s}[{i}]), .{out_c_out}({ci_out}[{i}])	// Outputs\n")
+				# output.append(f"	);\n")
 			elif(i == n-1):
-				output.append(f"	fa_1b fa{i}(.a({a}[{i}]), .b({b}[{i}]), .c_in(ci_out[{i-1}]), .c_out({c_out}), .s({s}[{i}]));\n")
+				output.append(use_fa(i , (in_a, a, i), (in_b, b, i), (in_c_in, ci_out, i-1), (out_s, s, i), (out_c_out, c_out, -1)))
+				# output.append(f"	fa_1b fa{i}(\n")
+				# output.append(f"		.{in_a}({a}[{i}]), .{in_b}({b}[{i}]), .{in_c_in}({ci_out}[{i-1}]), //Inputs\n")
+				# output.append(f"		.{out_s}({s}[{i}]), .{out_c_out}({c_out})	// Outputs\n")
+				# output.append(f"	);\n")
 			else:
-				output.append(f"	fa_1b fa{i}(.a({a}[{i}]), .b({b}[{i}]), .c_in(ci_out[{i-1}]), .c_out(ci_out[{i}]), .s({s}[{i}]));\n")
+				output.append(use_fa(i , (in_a, a, i), (in_b, b, i), (in_c_in, ci_out, i-1), (out_s, s, i), (out_c_out, ci_out, i)))
+				# output.append(f"	fa_1b fa{i}(\n")
+				# output.append(f"		.{in_a}({a}[{i}]), .{in_b}({b}[{i}]), .{in_c_in}({ci_out}[{i-1}]), //Inputs\n")
+				# output.append(f"		.{out_s}({s}[{i}]), .{out_c_out}({ci_out}[{i}])	// Outputs\n")
+				# output.append(f"	);\n")
 		
 	elif(n == 1): # fa_1b operations
 		output.append("	wire [2:0] net;\n\n")
---
+++ Python-Verilog-Export/functions.py
@@ -0,0 +1,60 @@
+def use_ha(index, a = ("a", "passed_in", -1), b = ("b", "passed_in", -1),
+	s = ("s", "passed_in", -1), c_out = ("c_out", "passed_in", -1)):
+
+	output = f"\tha ha{index}(\n"
+	if(a[2] == -1):
+		output += f"\t\t.{a[0]}({a[1]}), "
+	else:
+		output += f"\t\t.{a[0]}({a[1]}[{a[2]}]), "
+		
+	if(b[2] == -1):
+		output += f".{b[0]}({b[1]}), //Inputs\n"
+	else:
+		output += f".{b[0]}({b[1]}[{b[2]}]), //Inputs\n"
+
+	if(s[2] == -1):
+		output += f"\t\t.{s[0]}({s[1]}), "
+	else:
+		output += f"\t\t.{s[0]}({s[1]}[{s[2]}]), "
+
+	if(c_out[2] == -1):
+		output += f".{c_out[0]}({c_out[1]}) //Outputs \n"
+	else:
+		output += f".{c_out[0]}({c_out[1]}[{c_out[2]}]) //Outputs \n"
+	
+	output += f"\t);\n"
+	return output
+
+def use_fa(index, a = ("a", "passed_in", -1), b = ("b", "passed_in", -1), c_in = ("c_in", "passed_in", -1), 
+	s = ("s", "passed_in", -1), c_out = ("c_out", "passed_in", -1)):
+
+	output = f"\tfa_1b fa{index}(\n"
+	if(a[2] == -1):
+		output += f"\t\t.{a[0]}({a[1]}), "
+	else:
+		output += f"\t\t.{a[0]}({a[1]}[{a[2]}]), "
+		
+	if(b[2] == -1):
+		output += f".{b[0]}({b[1]}), "
+	else:
+		output += f".{b[0]}({b[1]}[{b[2]}]), "
+	
+	if(c_in[2] == -1):
+		output += f".{c_in[0]}({c_in[1]}), //Inputs\n"
+	else:
+		output += f".{c_in[0]}({c_in[1]}[{c_in[2]}]), //Inputs\n"
+
+	if(s[2] == -1):
+		output += f"\t\t.{s[0]}({s[1]}), "
+	else:
+		output += f"\t\t.{s[0]}({s[1]}[{s[2]}]), "
+
+	if(c_out[2] == -1):
+		output += f".{c_out[0]}({c_out[1]}) //Outputs \n"
+	else:
+		output += f".{c_out[0]}({c_out[1]}[{c_out[2]}]) //Outputs \n"
+	
+	output += f"\t);\n"
+	return output
+
+# print(use_fa(0, ("a", "a", 0), ("b", "b", 0), ("c_in", "c_in", -1), ("s", "s", 0), ("c_out", "ci_out", 0)))
